ef878d4a26633d5e7417f9f5e0cc60053542e0c3
brianyen <brianyen1000@gmail.com>
2025-12-21 19:44:46 -0800

    very basic IO

--- TextEdit.java
+++ TextEdit.java
@@ -1,31 +1,34 @@
 import java.util.ArrayList;
+import java.lang.StringBuilder;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.FileReader;
 import java.nio.file.FileAlreadyExistsException;
 
 public class TextEdit {
-    private final static int DEFAULT_SIZE = 25;
-
     private File f;
-    private ArrayList<String> buff;
+    private ArrayList<char[]> buff;
     private int[] cursor;
+    private int topLine;
 
-    public TextEdit(String fName) throws FileAlreadyExistsException {
+    public TextEdit(String fName) throws IOException {
         f = new File(fName);
         cursor = new int[2];
+        topLine = 0;
         buff = new ArrayList<>();
 
         if (f.isDirectory()) {
-            throw new FileAlreadyExistsException("File is a directory");
+            System.out.println("File is a directory");
         } else if (f.isFile()) {
             BufferedReader reader = new BufferedReader(new FileReader(f));
             String line;
-            while (line = reader.readLine() != null) {
-                buff.add(line);
+            while ((line = reader.readLine()) != null) {
+                buff.add(line.toCharArray());
             }
+            reader.close();
         }
     }
 
@@ -36,6 +39,41 @@ public class TextEdit {
         }
         String fName = args[0];
         TextEdit tEdit = new TextEdit(fName);
+        tEdit.output();
+    }
+
+    private void output() {
+        System.out.println(CLEAR_STRING);
+        int numLines = Math.min(DEFAULT_SIZE, buff.size() - topLine);
+        StringBuilder outBuilder = new StringBuilder();
+        for (int i = 0; i < numLines; i++) {
+            char[] line = buff.get(topLine + i);
+            if (topLine + i == cursor[0]) {
+                line[cursor[1]] = '\u2588';
+            }
+            outBuilder.append(line);
+        }
+        System.out.println(outBuilder);
+    }
+
+    private boolean save() throws IOException {
+        if (f.isDirectory()) {
+            return false;
+        }
+        if (!f.isFile()) {
+            f.createNewFile();
+        }
+
+        if (f.isFile()) {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(f));
+            for (char[] l : buff) {
+                writer.write(String.valueOf(l));
+                writer.newLine();
+            }
+            writer.close();
+            return true;
+        }
+        return false;
     }
 
     // move cursor left
@@ -46,4 +84,7 @@ public class TextEdit {
     // insert newline
     // output buff
     // save
+
+    private static final String CLEAR_STRING = "\033[H\033[2J";
+    private final static int DEFAULT_SIZE = 25;
 }
